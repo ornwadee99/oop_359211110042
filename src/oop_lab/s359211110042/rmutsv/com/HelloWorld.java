7240a9bab3e22d3a13de5300fbcc88124c70bc08
ornwadee99 <ornwadee28493@gmail.com>
2017-11-19 09:49:04 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110042/rmutsv/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package oop_lab.s359211110042.rmutsv.com;
+
+public class HelloWorld
+{
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world, I'm BAM");
+    System.out.println("My name is Ornwadee Hamtanon");
+
+
+}//main
+
+}//class
